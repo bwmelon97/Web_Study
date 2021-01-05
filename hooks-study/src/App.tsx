6dd7e7b832407b1dfcbeece5d309b5acb94e6705
Soogeun Park <bwmelon97@naver.com>
2021-01-05 13:49:25 +0000

    create example LoginInput (초기버전)

--- hooks-study/src/App.tsx
+++ hooks-study/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+import { LoginInput } from "./components";
+
+
 function App() {
 
     return (
         <div>
-
+            <LoginInput />
         </div>
     )
 }
