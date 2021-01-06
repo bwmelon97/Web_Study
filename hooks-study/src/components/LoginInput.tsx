2a4e20f47b49c18e3c05444525f8a68c156d3a4c
Soogeun Park <bwmelon97@naver.com>
2021-01-06 14:34:30 +0000

    update LoginInput Component study useMemo

--- hooks-study/src/components/LoginInput.tsx
+++ hooks-study/src/components/LoginInput.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 
 const testVaildID = (id: string): boolean => {
-    console.log('id' + id);
+    console.log('id: ' + id);
     return id.search('@') !== -1;   // id에 @가 있으면 true 없으면 false
 }
 
 const testValidPW = (pw: string): boolean => {
-    console.log('pw' + pw);
+    console.log('pw: ' + pw);
     return pw.length >= 6;
 }
 
@@ -16,15 +16,17 @@ function LoginInput () {
     const [id, setID] = useState<string>('');
     const [pw, setPW] = useState<string>('');
 
-    const isValidID = testVaildID(id);
-    const isVaildPW = testValidPW(pw);
-
+    // const isValidID = testVaildID(id);
+    // const isVaildPW = testValidPW(pw);
+    const isValidID = useMemo(() => testVaildID(id), [id]);
+    const isValidPW = useMemo(() => testValidPW(pw), [pw]);
+ 
     return (
         <div>
             <input type='text' id='id' value={id} onChange={(e) => setID(e.target.value)} />
             {!isValidID && '이메일 형식의 아이디를 입력해주세요'}
             <input type='text' id='pw' value={pw} onChange={(e) => setPW(e.target.value)} />
-            {!isVaildPW && '비밀번호는 6자리 이상으로 해주세요'}
+            {!isValidPW && '비밀번호는 6자리 이상으로 해주세요'}
         </div>
     )
 
