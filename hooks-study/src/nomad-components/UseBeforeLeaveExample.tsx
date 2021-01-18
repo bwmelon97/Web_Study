c4c067987624cd9d85796e52504a3d7cb44c4a7c
Soogeun Park <bwmelon97@naver.com>
2021-01-18 14:13:16 +0000

    create useBeforeLeave

--- hooks-study/src/nomad-components/UseBeforeLeaveExample.tsx
+++ hooks-study/src/nomad-components/UseBeforeLeaveExample.tsx
@@ -1,18 +1,16 @@
 import React, {  } from "react";
+import useBeforeLeave from "../nomad-hooks/useBeforeLeave";
 
 
 function UseBeforeLeaveExample () {
 
-    const preventLeave = (event: BeforeUnloadEvent) => {
-        event.preventDefault();
-        event.returnValue = '';
-    }
-
-    window.addEventListener('beforeunload', preventLeave);
+    const { lockLeaving, unLockLeaving } = useBeforeLeave();
 
     return (
         <>
             No way !
+            <button onClick={lockLeaving} > 잠금 </button>
+            <button onClick={unLockLeaving} > 해제 </button>
         </>
     )
 }
---
+++ hooks-study/src/nomad-hooks/useBeforeLeave.ts
@@ -0,0 +1,17 @@
+function useBeforeLeave () {
+
+    const preventLeave = (event: BeforeUnloadEvent) => {
+        event.preventDefault();
+        event.returnValue = '';
+    }
+
+    const lockLeaving = () => window.addEventListener('beforeunload', preventLeave);
+    const unLockLeaving = () => window.removeEventListener('beforeunload', preventLeave);
+
+    return {
+        lockLeaving,
+        unLockLeaving
+    }
+}
+
+export default useBeforeLeave;
