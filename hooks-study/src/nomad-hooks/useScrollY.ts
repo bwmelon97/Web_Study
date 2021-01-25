ade12e2e70b03f49154441d74548b4333a763edd
Soogeun Park <bwmelon97@naver.com>
2021-01-25 07:41:22 +0000

    useScrollY를 window와 component 모두 사용가능하도록 함 - window만 테스트 완료

--- hooks-study/src/nomad-hooks/useScrollY.ts
+++ hooks-study/src/nomad-hooks/useScrollY.ts
@@ -1,37 +1,43 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 
-function useScrollY () {
+function useScrollY <T extends HTMLElement> (
+    kind: 'window' | 'component' = 'window'
+) {
     
+    const isWindow = kind === 'window';
+    const element = useRef<T>(null);
+
     /* Scroll_Y : 스크롤바가 위치한 값 */
-    const [scrollY, setScrollY] = useState<number>(window.scrollY);
+    const getScrollY = useCallback(() => isWindow ? window.scrollY : element.current?.scrollTop, [isWindow]) 
+    const [scrollY, setScrollY] = useState<number>(getScrollY() as number);
     useEffect(() => {
-        const handleScroll = () => setScrollY(window.scrollY)
+        const handleScroll = () => setScrollY(getScrollY() as number) ;
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         }
-    }, [])
+    }, [getScrollY])
 
-    /* 브라우저 창의 높이 값 */
-    const [innerHeight, setInnerHeight] = useState<number>(window.innerHeight);
+    /* 브라우저 창의 높이 값 | 오브젝트의 보여지는 부분의 높이 값 */
+    const getInnerHeight = useCallback(() => isWindow ? window.innerHeight : element.current?.clientHeight, [isWindow]) 
+    const [innerHeight, setInnerHeight] = useState<number>(getInnerHeight() as number);
     useEffect(() => {
-        const handleResize = () => setInnerHeight(window.innerHeight);
+        const handleResize = () => setInnerHeight(getInnerHeight() as number);
         window.addEventListener('resize', handleResize);
         return () => {
             window.removeEventListener('resize', handleResize);
         }
-    }, [])
+    }, [getInnerHeight])
 
-    /* 전체 Body의 높이 값 */
-    const [scrollHeight, setScrollHeight] = useState<number>(document.body.scrollHeight);
+    /* 전체 Body의 높이 값 | 오브젝트의 전체 높이 값 */
+    const getScrollHeight = useCallback(() => isWindow ? document.body.scrollHeight : element.current?.scrollHeight, [isWindow]) 
+    const [scrollHeight, setScrollHeight] = useState<number>(getScrollHeight() as number);
     // eslint-disable-next-line
-    useEffect(() => {
-        console.log('[useScrollY] render !')
-        setScrollHeight(document.body.scrollHeight);
-    })
+    useEffect(() => setScrollHeight(getScrollHeight() as number))
 
     return {
+        element,
         scrollY, innerHeight, scrollHeight
     }
 }
