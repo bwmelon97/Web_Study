36c13a74b388bbc6bbb6bfa45d72afeba2e49a56
Soogeun Park <bwmelon97@naver.com>
2021-01-27 14:57:59 +0000

    create useAxios

--- hooks-study/src/nomad-components/UseAxiosExample.tsx
+++ hooks-study/src/nomad-components/UseAxiosExample.tsx
@@ -1,12 +1,17 @@
 import React, {  } from 'react';
+import { useAxios } from '../nomad-hooks';
 
 
 function UseAxiosExample () {
 
+    const { data, loading } = useAxios({ url: 'https://aws.random.cat/meow'});
+
+    console.log(data)
 
     return (
         <div>
-
+            <h1> {data && data} </h1>
+            <h2> {loading && 'Loading' } </h2>
         </div>
     )
 }
--- hooks-study/src/nomad-hooks/useAxios.ts
+++ hooks-study/src/nomad-hooks/useAxios.ts
@@ -1,8 +1,48 @@
-import {  } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
-function useAxios () {
+type AxiosOptions = {
+    url: string;
+}
 
+type State = {
+    loading: boolean,
+    data: Object | null,
+    error: Error | null
+}
+
+function useAxios ( { url }: AxiosOptions ) {
+
+    const [state, setState] = useState<State>({
+        loading: true,
+        data: null,
+        error: null
+    });
+
+    useEffect(() => {
+        axios.get( url )
+        .then((res) => {
+            setState( s => {
+                return {
+                    ...s,
+                    loading: false,
+                    data: res.data
+                }
+            })
+        })
+        .catch((error) => {
+            setState( s => {
+                return {
+                    ...s,
+                    loading: false,
+                    error
+                }
+            })
+        })
+    }, [])
+
+    return state;
 }
 
 export default useAxios;
