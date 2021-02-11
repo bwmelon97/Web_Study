c2f293808ee7b4a42ae8e04212477b7d0f270b0d
Soogeun Park <bwmelon97@naver.com>
2021-02-11 14:49:43 +0000

    [apollo-app] get Movie Data for Detail Page

--- apollo-app/src/pages/Detail.tsx
+++ apollo-app/src/pages/Detail.tsx
@@ -1,16 +1,63 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
 
+
+const GET_MOVIE_BY_ID = gql`
+    query getMovie($movie_id: Int!) {
+        movie(movie_id: $movie_id) {
+            id
+            title
+            rating
+            genres
+            summary
+            medium_cover_image
+        }
+    }
+`;
+
+//type Movie = {
+//    id: number;
+//    title: string;
+//    rating: number;
+//    genres: string[];
+//    summary: string;
+//    medium_cover_image: string;
+//}
 
 function Detail () {
 
     const { id } = useParams<{id?: string}>();
-    console.log(id)
+    //if (!id) return <p>It's invalid page :(</p>
+
+    const { loading, error, data } = useQuery(GET_MOVIE_BY_ID, {
+        variables: { movie_id: +(id as string) }
+    })
+
+    if (loading) return <h1> loading... </h1>
+    if (error) return <p> {error.message} </p>
+
+    console.log(id);
+    console.log(data);
+
+    const {
+        movie: {
+            title,
+            rating,
+            genres,
+            summary,
+            medium_cover_image
+        }
+    } = data
 
     return (
-        <h1>
-            Detail
-        </h1>
+        <div>
+            <h1> {title} </h1>
+            <h3> {rating} </h3>
+            <img src={medium_cover_image} alt='Movie Cover' />
+            { genres.map((g: string, idx: number) => <span key={idx} > {g} </span> ) }
+            <p> {summary} </p>
+        </div>
     )
 }
 
