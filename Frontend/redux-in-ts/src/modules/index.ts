7089a91b5410dc42b067f51e6d1a3bcb866c4078
Soogeun Park <bwmelon97@naver.com>
2021-04-06 13:37:06 +0000

    f/redux: React앱 에서 Redux를 사용하기 위한 최소한의 세팅

---
+++ Frontend/redux-in-ts/src/modules/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    /* Reducer List */
+})
+
+export default rootReducer;
+
+/* 루트 State의 타입입 */
+export type RootState = ReturnType<typeof rootReducer>
